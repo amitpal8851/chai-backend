37327a00b446da41b3c29f9e7b848fb577d44bc6
Amit Pal <amitpalmi0@gmail.com>
2026-03-10 02:46:45 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import e from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = e();
+
+
+//for cors
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(e.json({limit: "16kb"}));
+app.use(e.urlencoded({extended: true, limit: "16kb"}));
+app.use(e.static("public"));
+app.use(cookieParser())
+
+
+export { app };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+// for promises
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler};
+
+
+
+
+
+// this is for try catch
+
+// for higher order function
+/* const asyncHandler = (fn) => {async() => {}} */
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
